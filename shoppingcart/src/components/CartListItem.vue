955d8ab75fbc534ad82aafad29e9de8ba57bba1c
Tomas <cernytomas@hotmail.com>
2021-08-23 10:01:48 +0200

    cartItems setup

---
+++ shoppingcart/src/components/CartListItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="box">
+    <div class="cart-item__details">
+      <p class="is-inline">{{cartItem.product_name}}</p>
+      <div>
+        <span class="cart-item--price has-text-info has-text-weight-bold">
+        sek {{cartItem.retail_price}} X <!-- {{cartItem.quantity}} -->
+        </span>
+
+        <span>
+          <i class="fa fa-arrow-circle-up cart-item__modify" @click="addCartItem(cartItem)"></i>
+          <i class="fa fa-arrow-circle-down cart-item__modify" @click="removeCartItem(cartItem)"></i>
+        </span>
+      </div>
+
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+   props: ["cartItem"]
+    
+}
+</script>
---
+++ shoppingcart/src/views/CartList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="cart">
+    <div class="cart--header has-text-centered">
+      <i class="fa fa-2x fa-shopping-cart"></i>
+    </div>
+    <p v-if="!cartItems.length" class="cart-empty-text has-text-centered">
+      Add some items to the cart!
+    </p>
+    <ul>
+      <li class="cart-item" v-for="cartItem in cartItems" :key="cartItem.id">
+          <CartListItem :cartItem="cartItem"/>
+      </li>
+      <div class="notification is-success">
+        <button class="delete"></button>
+        <p>
+          Total Quantity:
+          <span class="has-text-weight-bold">{{ cartQuantity }}</span>
+        </p>
+      </div>
+      <br>
+    </ul>
+    <div class="buttons">
+    <button :disabled="!cartItems.length" class="button is-info">
+      Checkout (<span class="has-text-weight-bold">sek{{ cartTotal }}</span>)
+    </button>
+
+ <button class="button is-danger is-outlined" @click="removeAllCartItems">
+    <span>Delete All items</span>
+    <span class="icon is-small">
+      <i class="fas fa-times"></i>
+    </span>
+  </button>
+       </div>
+  </div>
+</template>
+<script>
+import CartListItem from '../components/CartListItem.vue'
+export default {
+    components:{
+        CartListItem
+    },
+    computed:{
+        cartItems(){
+            return this.$store.state.cartItems
+        }
+    },
+    created(){
+        this.$store.dispatch('updateCartItems')
+    }
+    
+}
+</script>
